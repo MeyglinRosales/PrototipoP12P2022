bc55cf38bdc0793c1538364ede9cc996e4e1b8ce
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-04-21 15:47:01 -0600

    se ha agregado la parte de usurios de las clases

--- SegundoParcial/include/Usuarios.h
+++ SegundoParcial/include/Usuarios.h
@@ -1,5 +1,7 @@
 #ifndef USUARIOS_H
 #define USUARIOS_H
+#include <string>
+using namespace std;
 
 
 class Usuarios
@@ -7,10 +9,14 @@ class Usuarios
     public:
         Usuarios();
         virtual ~Usuarios();
-
+        bool loginUsuarios();
+        void menuUsuarios();
+        void obtenerUser(string);
+        string userP;
     protected:
 
     private:
 };
 
 #endif // USUARIOS_H
+
